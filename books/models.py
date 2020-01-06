5c82c6c3c925915a60707bd0f5a56df9176c417f
mkzr9 <mohammadkazemire9@gmail.com>
2020-01-06 19:45:30 +0330

    init

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    name = models.CharField(verbose_name="نام",max_length=100)
+    auther = models.ForeignKey("Author",on_delete=models.CASCADE)
+
+class Author(models.Model):
+    fname = models.CharField(verbose_name="نام کوچک نویسنده",max_length=100)
+    lname = models.CharField(verbose_name="نام خانوادگی نویسنده",max_length=100)
+    birthday = models.DateTimeField(verbose_name="تاریخ تولد")
--- library/settings.py
+++ library/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'books.apps.BooksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
