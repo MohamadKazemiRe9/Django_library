c0e541d9ed85315b84b2c55067af25d25dc1696d
mkzr9 <mohammadkazemire9@gmail.com>
2020-01-12 16:12:53 +0330

    init

--- books/models.py
+++ books/models.py
@@ -4,8 +4,18 @@ from django.db import models
 class Books(models.Model):
     name = models.CharField(verbose_name="نام",max_length=100)
     auther = models.ForeignKey("Author",on_delete=models.CASCADE)
+    vote = models.FloatField(default=0,verbose_name="امتیاز کتاب",)
+    vote_number = models.IntegerField(default=0,verbose_name="تعداد امتیازات")
+
+    def __str__(self):
+        return str(self.name)
+    
 
 class Author(models.Model):
     fname = models.CharField(verbose_name="نام کوچک نویسنده",max_length=100)
     lname = models.CharField(verbose_name="نام خانوادگی نویسنده",max_length=100)
     birthday = models.DateTimeField(verbose_name="تاریخ تولد")
+
+    def __str__(self):
+        return str(self.fname+" "+self.lname)
+    
--- books/views.py
+++ books/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render , get_object_or_404 , get_list_or_404
+from django.http import HttpResponse ,response,Http404
+from .models import Books
+from django.views import generic
 
-# Create your views here.
+
 def booksInfo(request):
-    return HttpResponse("books")
+    sort = get_list_or_404(Books.objects.order_by('name'))
+    context = {"hot_books":sort}
+    return render(request,"books/index.html",context)
+
+# class IndexView(generic.ListView):
+#     model = Books
+#     template_name = "books/index.html"
+
+#     def get_queryset(self):
+#         return Books.objects.order_by('name')
+
+
+def details(request,book_id):
+    book_details = get_object_or_404(Books,id = book_id)
+    return render(request,"books/details.html",{"book":book_details})
+
+def vote(request,id):
+    book = get_object_or_404(Books,pk = id)
+    book.vote_number += 1
+    if(book.vote_number == 1):
+        book.vote += int(request.POST["vote"])
+    else:
+        book.vote = round(float((book.vote*(book.vote_number-1)) + float(request.POST["vote"]))/(book.vote_number),2)
+    book.save()
+    return render(request,"books/vote.html",{"book":book})
