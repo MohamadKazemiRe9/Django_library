f70aa2dec87a7762eae4ad2bbb4caa9ba66a04a8
mkzr9 <mohammadkazemire9@gmail.com>
2020-01-13 11:43:15 +0330

    init

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -4,9 +4,10 @@ from books.models import Books
 class BooksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Books
-        fields = [
-            'pk',
-            'name',
-            'auther',
-        ]
+        # fields = [
+        #     'pk',
+        #     'name',
+        #     'auther',
+        # ]
+        fields ="__all__"
         read_only_fields = ["pk"]
